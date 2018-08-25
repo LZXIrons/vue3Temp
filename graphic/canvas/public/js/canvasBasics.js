5f598d6adbc563f2ec8ea31af5a00050aa3fc3d6
liu_yong_company_pc <liuyongfov@163.com>
2018-08-25 15:26:46 +0800

    draw rect

--- graphic/canvas/public/js/canvasBasics.js
+++ graphic/canvas/public/js/canvasBasics.js
@@ -14,15 +14,20 @@
         drawOneLine();
         /*画三到折线*/
         drawThreeLine();
+        /*用线条画一个矩形*/
+        drawOneRectangleWithLine();
+        fillOneRectangleWithLine();
+        /*重复回形正方形*/
+        drawBackRect();
     },
     drawOneLine=function(){
         myctx.beginPath();
         myctx.moveTo(10,2);
         myctx.lineTo(100,2);
+        myctx.closePath();
         myctx.lineWidth = 4;
         myctx.strokeStyle = "#f00";
         myctx.stroke();
-        myctx.closePath();
     },
     drawThreeLine=function(){
         myctx.beginPath();
@@ -32,7 +37,6 @@
         myctx.lineWidth = 3;
         myctx.strokeStyle = "#f00";
         myctx.stroke();
-        myctx.closePath();
 
         myctx.beginPath();
         myctx.moveTo(50,10);
@@ -41,7 +45,6 @@
         myctx.lineWidth = 3;
         myctx.strokeStyle = "#ff0";
         myctx.stroke();
-        myctx.closePath();
 
         myctx.beginPath();
         myctx.moveTo(90,10);
@@ -50,7 +53,44 @@
         myctx.lineWidth = 3;
         myctx.strokeStyle = "#000";
         myctx.stroke();
+    },
+    drawOneRectangleWithLine=function(){
+        myctx.beginPath();
+        myctx.moveTo(220,10);
+        myctx.lineTo(320,10);
+        myctx.lineTo(320,100);
+        myctx.lineTo(220,100);
         myctx.closePath();
+        myctx.lineWidth=10;
+        myctx.strokeStyle="#FF83FA";
+        myctx.stroke();
+    },
+    fillOneRectangleWithLine=function(){
+        myctx.beginPath();
+        myctx.moveTo(220,110);
+        myctx.lineTo(320,110);
+        myctx.lineTo(320,200);
+        myctx.lineTo(220,200);
+        myctx.closePath();
+        myctx.fillStyle="#FFD700";
+        myctx.lineWidth=10;
+        myctx.strokeStyle="#FFDAB9";
+        myctx.fill();
+        myctx.stroke();
+    },
+    drawBackRect=function(){
+        for(var i=0;i<10;i++){
+            myctx.beginPath();
+            myctx.rect(450-i*10,100-i*10,2*i*10,2*i*10);
+            myctx.lineWidth=10;
+            if(i%2==0){
+                myctx.strokeStyle="#FF1493";
+            }else{
+                myctx.strokeStyle="#FF6A6A";
+            }
+            myctx.stroke();
+            myctx.closePath();
+        }
     },
     windowResize = function() {
         _w = _body.offsetWidth;
