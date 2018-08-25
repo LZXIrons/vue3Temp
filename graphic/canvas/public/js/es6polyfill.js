12a2d7bed2964abffe68e219bb7e3584155b4274
liu_yong_company_pc <liuyongfov@163.com>
2018-08-25 15:26:23 +0800

    study tools and es6polyfill

--- graphic/canvas/public/js/es6polyfill.js
+++ graphic/canvas/public/js/es6polyfill.js
@@ -2,3 +2,37 @@
 Number.isInteger = Number.isInteger || function(value){
     return typeof value==='number' && isFinite(value) && Math.floor(value)===value;
 };
+String.prototype.repeat = String.prototype.repeat || function(count){
+    if (this == null) {
+        throw new TypeError('can\'t convert '+this+' to object');
+    }
+    count=Math.floor(count);
+    if(count!==count){
+        return '';
+    }
+    if (count < 0) {
+      throw new RangeError('repeat count must be non-negative');
+    }
+    if (count == Infinity) {
+      throw new RangeError('repeat count must be less than infinity');
+    }
+    var str=this.toString();
+    if (str.length == 0 || count == 0) {
+      return '';
+    }
+    if (str.length * count >= 1 << 28) {
+      throw new RangeError('repeat count must not overflow maximum string size');
+    }
+    var rpt = '';
+    for (;;) {
+        if ((count & 1) == 1) {
+            rpt += str;
+        }
+      count >>>= 1;
+      if (count == 0) {
+        break;
+      }
+      str += str;
+    }
+    return rpt;
+};
--- graphic/canvas/public/js/tools.js
+++ graphic/canvas/public/js/tools.js
@@ -1,4 +1,110 @@
 "use strict";
+var PubSub=(function(global){
+    var defaultSpace="default",
+        nameSpace = {},
+        _arrslice=Array.prototype.slice;
+    function EventObj(_name){
+        this._name=_name;
+        this.cache={};
+    }
+    EventObj.prototype={
+        constructor:EventObj,
+        one:function(key,fn,notlast){
+            this.cache[key] = this.cache[key] || {};
+            var _cacheByKey=this.cache[key],
+                args=_arrslice.call(arguments),
+                _ckey=getUuid();
+            if(!notlast && _cacheByKey.last){
+                fn.apply(this,args.slice(3).concat(_cacheByKey.last));
+                return true;
+            }
+            _cacheByKey[_ckey]=args;
+            _cacheByKey[_ckey].once=true;
+        },
+        on:function(key,fn,notlast){
+            this.cache[key] = this.cache[key] || {};
+            var _cacheByKey=this.cache[key],
+                _ckey=getUuid();
+            _cacheByKey[_ckey]=_arrslice.call(arguments);
+            if(!notlast && _cacheByKey.last){
+                fn.apply(this,_cacheByKey[_ckey].slice(3).concat(_cacheByKey.last));
+            }
+        },
+        emit:function(key,notlast){
+            this.cache[key] = this.cache[key] || {};
+            var params=_arrslice.call(arguments,2);
+            if(!notlast){
+                this.cache[key].last=params;
+            }
+            this.carryOut(this.cache[key],params);
+        },
+        remove:function(key,fn){
+            if(fn){
+                var _cacheByKey= this.cache[key],
+                    ccnum=0;
+                for(var c in _cacheByKey){
+                    if(_cacheByKey.hasOwnProperty(c)){
+                        if(_cacheByKey[c][1]==fn){
+                            delete _cacheByKey[c];
+                            continue;
+                        }
+                        ++ccnum;
+                    }
+                }
+                !ccnum && (delete this.cache[key]);
+            }else if(key){
+                delete this.cache[key];
+            }else{
+                delete nameSpace[this._name];
+                return;
+            }
+            var keynum=0;
+            for(var i in this.cache){
+                if(!this.cache.hasOwnProperty(i)){
+                    continue;
+                }
+                ++keynum;
+                break;
+            }
+            !keynum && (delete nameSpace[this._name]);
+        },
+        carryOut:function(_cacheByKey,params){
+            for(var _ckey in _cacheByKey){
+                if(!_cacheByKey.hasOwnProperty(_ckey)){
+                    continue;
+                }
+                var _cck=_cacheByKey[_ckey];
+                if(!_cck[1]){
+                    continue;
+                }
+                _cck[1].apply(this,_cck.slice(3).concat(params));
+                _cck.once && delete _cacheByKey[_ckey];
+            }
+        }
+    };
+    return {
+        space:function(_name){
+            _name = _name || defaultSpace;
+            return nameSpace[_name] || (nameSpace[_name] = new EventObj(_name));
+        },
+        one:function(){
+            var _temp=this.space();
+            return _temp.one.apply(_temp,_arrslice.call(arguments));
+        },
+        on:function(){
+            var _temp=this.space();
+            return _temp.on.apply(_temp,_arrslice.call(arguments));
+        },
+        emit:function(){
+            var _temp=this.space();
+            return _temp.emit.apply(_temp,_arrslice.call(arguments));
+        },
+        remove:function(){
+            var _temp=this.space();
+            return _temp.remove.apply(_temp,_arrslice.call(arguments));
+        }
+    };
+})(this);
 /*
   *brower utils
   @author liuyong
@@ -72,9 +178,17 @@
             return elemTop;
         }
     };
+
     function Tools() {};
     Tools.prototype = {
         constructor: Tools,
+        extend : function(child,father){
+            var obj=function(){};
+            obj.prototype=father.prototype;
+            child.prototype=new obj();
+            child.prototype.constructor = child;
+            child.uber=father.prototype;
+        },
         jsonExtend : function(child, father, deep, attrs) {
             for (var i in father) {
                 if (attrs && attrs.indexOf(i) == -1) {
@@ -90,6 +204,15 @@
                 }
             }
             return child;
+        },
+        querySearch : function(){
+            var _sarr=location.search.substring(1).split('&'),
+                _sobj={};
+            for(var i=0,len=_sarr.length;i<len;i++){
+                var _sasingle=_sarr[i].split('=');
+                _sobj[_sasingle[0]]=_sasingle[1];
+            }
+            return _sobj;
         },
         formatDate : function(time, format, errtxt) {
             if (!time) {
