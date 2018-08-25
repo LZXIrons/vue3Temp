7e6e131963388f8247fd96c959bb98caeb1d1d80
liu_yong_company_pc <liuyongfov@163.com>
2018-08-25 14:11:29 +0800

    study canvas basics draw three broken line

--- graphic/canvas/public/js/canvasBasics.js
+++ graphic/canvas/public/js/canvasBasics.js
@@ -12,13 +12,45 @@
         window.addEventListener('resize', windowResize);
         /*画一条线*/
         drawOneLine();
+        /*画三到折线*/
+        drawThreeLine();
     },
     drawOneLine=function(){
+        myctx.beginPath();
+        myctx.moveTo(10,2);
+        myctx.lineTo(100,2);
+        myctx.lineWidth = 4;
+        myctx.strokeStyle = "#f00";
+        myctx.stroke();
+        myctx.closePath();
+    },
+    drawThreeLine=function(){
+        myctx.beginPath();
         myctx.moveTo(10,10);
-        myctx.lineTo(100,10);
+        myctx.lineTo(100,100);
+        myctx.lineTo(10,200);
         myctx.lineWidth = 3;
         myctx.strokeStyle = "#f00";
         myctx.stroke();
+        myctx.closePath();
+
+        myctx.beginPath();
+        myctx.moveTo(50,10);
+        myctx.lineTo(150,100);
+        myctx.lineTo(50,200);
+        myctx.lineWidth = 3;
+        myctx.strokeStyle = "#ff0";
+        myctx.stroke();
+        myctx.closePath();
+
+        myctx.beginPath();
+        myctx.moveTo(90,10);
+        myctx.lineTo(190,100);
+        myctx.lineTo(90,200);
+        myctx.lineWidth = 3;
+        myctx.strokeStyle = "#000";
+        myctx.stroke();
+        myctx.closePath();
     },
     windowResize = function() {
         _w = _body.offsetWidth;
