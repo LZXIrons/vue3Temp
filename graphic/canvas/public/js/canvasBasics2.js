20dc5e3bf46c103803cf6c8160a32d23c2a7643e
liu_yong_company_pc <liuyongfov@163.com>
2018-09-06 16:34:46 +0800

    study canvas basics transformation

--- graphic/canvas/public/js/canvasBasics2.js
+++ graphic/canvas/public/js/canvasBasics2.js
@@ -1,7 +1,7 @@
 "use strict";
 ;(function(window) {
     var mycanvas = _.dom.qid('mycanvas'),
-    myctx = mycanvas.getContext('2d'),
+    cxt = mycanvas.getContext('2d'),
     _body = document.body,
     _w,
     //容器的宽度，也是canvas的宽度
@@ -10,6 +10,60 @@
         windowResize();
         _.dom.visualState(mycanvas,'visibility');
         window.addEventListener('resize', windowResize);
+        testTranslate();
+        testRotate();
+        testScale();
+        testTransform();
+    },
+    testTranslate=function(){
+        cxt.save();
+        cxt.beginPath();
+        cxt.fillStyle="#EE00EE";
+        cxt.translate(-100,-100);
+        cxt.fillRect(100,100,200,200);
+        cxt.restore();
+
+        cxt.save();
+        cxt.beginPath();
+        cxt.fillStyle="#EE0000";
+        cxt.translate(200,0);
+        cxt.fillRect(0,0,100,200);
+        cxt.restore();
+    },
+    testRotate=function(){
+        cxt.beginPath();
+        cxt.save();
+        cxt.fillStyle="#CD6839";
+        cxt.translate(350,50);
+        cxt.rotate(.25*Math.PI);
+        cxt.fillRect(0,0,50,50);
+        cxt.restore();
+        cxt.save();
+        cxt.translate(350,50);
+        cxt.rotate(.15*Math.PI);
+        cxt.fillRect(0,0,50,50);
+        cxt.restore();
+        cxt.closePath();
+    },
+    testScale=function(){
+        cxt.beginPath();
+        cxt.save();
+        cxt.strokeStyle="#B3EE3A";
+        cxt.translate(400,50);
+        cxt.scale(2,2);
+        cxt.rect(0,0,50,50);
+        cxt.stroke();
+        cxt.restore();
+        cxt.closePath();
+    },
+    testTransform=function(){
+        cxt.beginPath();
+        cxt.save();
+        cxt.setTransform(1,0.1,-0.1,1,30,10);
+        cxt.fillStyle="blue";
+        cxt.fillRect(500,0,120,100);
+        cxt.restore();
+        cxt.closePath();
     },
     windowResize = function() {
         _w = _body.offsetWidth;
