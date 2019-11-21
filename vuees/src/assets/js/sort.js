6ca8652ee0d479be5b2a8b814c8083e8708ed24b
liu_yong <liuyongfov@163.com>
2019-11-21 18:31:01 +0800

    quick sort

--- vuees/src/assets/js/sort.js
+++ vuees/src/assets/js/sort.js
@@ -1,65 +1,134 @@
-/**
- * @description: 两个变量交换值
- * @param {any1,any2} 
- * @return: Array(any1,any2)
+/*
+ * @Author: LiuYong
+ * @Date: 2019-11-16 17:36:57
+ * @LastEditTime: 2019-11-21 16:20:41
+ * @LastEditors: Please set LastEditors
+ * @Description: 排序算法
  */
-function exchangeValue(variable1,variable2){
-    let tmp=variable2;
-    variable2=variable1;
-    variable1=tmp;
-    return [variable1,variable2];
+
+/**
+ * @description: 对象、数组的两个key交换值
+ * @param {Object,i,j} 
+ * @return: void
+ */
+function exchangeArray(obj,i,j){
+    let tmp=obj[i];
+    obj[i] = obj[j];
+    obj[j] = tmp;
 }
 
 /**
- * @description: 快速排序
- * @param {Array,left,right} 
- * @return: Array
+ * @description: 验证数组是否升序
+ * @param {Array} 
+ * @return: Boolean
  */
-function quickSort(arr,left=0,right){
+function verifyArrayOrderlyAsc(arr){
     if(!Array.isArray(arr)){
-        return TypeError('参数必须是数组');
+        throw TypeError('param must be a Array');
     }
-    if(arr.length <= 1){
-        return arr;
-    }
-    right=arr.length-1;
-    let mid=Math.floor(right/2);
-    while(true){
-        if(arr[left]>arr[mid]){
-            exchangeValue(arr[mid],arr[left]);
-            ++left;
+    let lastVal=arr[0];
+    for(let i=1,len=arr.length;i<len;i++){
+        if(lastVal>arr[i]){
+            return false;
         }
-        if(arr[right]<arr[mid]){
-            exchangeValue(arr[mid],arr[right]);
-            --right;
-        }
+        lastVal=arr[i];
     }
+    return true;
 }
 
 /**
- * @description: 快速排序
+ * @description: 验证数组是否降序
+ * @param {Array} 
+ * @return: Boolean
+ */
+function verifyArrayOrderlyDesc(arr){
+    if(!Array.isArray(arr)){
+        throw TypeError('param must be a Array');
+    }
+    let lastVal=arr[0];
+    for(let i=1,len=arr.length;i<len;i++){
+        if(lastVal<arr[i]){
+            return false;
+        }
+        lastVal=arr[i];
+    }
+    return true;
+}
+
+/**
+ * @description: 快速排序-升序,快速排序算法是不稳定的算法
  * @param {Array} 
  * @return: Array
  */
-function quickSort1(arr){
-    if(!Array.isArray(arr)){
-        return TypeError('参数必须是数组');
-    }
-    if(arr.length <= 1){
-        return arr;
-    }
-    let left = [],
-        right = [],
-        datum = arr.splice(0,1);
-    arr.forEach(val => {
-        if(val > datum){
-            right.push(val);
-        }else{
-            left.push(val);
+function quickSortAsc(arr) {
+    let sort=function(sarr, left=0, right=arr.length-1){
+        if(left<right && sarr.length>1){
+            //左边的索引小于右边的索引时，说明还需要排序，不然就是已经整理完了
+            let _left=left,
+                _right=right+1,
+                mid=Math.floor((left+right)/2),
+                pivot = sarr[mid];
+            //取无序数组中间一个数为基准，这里是取的Math.floor((left+right)/2)
+            //主要是如果数组本身就是有序的，那么取中间一个数做基准是最恰当的
+            for(let i=left;i<_right;i++){
+                if(sarr[i]<pivot){
+                    //把所有小于等于基准的数都放到数组前面去
+                    _left!=i && exchangeArray(sarr,_left,i);
+                    //移动pivot的索引，方便后面放到_left位置去
+                    if(_left==mid){
+                        mid=i;
+                    }
+                    ++_left;
+                }
+            }
+            //把所有小于基准的数都放到数组_left前面去了
+            //现在把基准放到数组_left的位置处
+            exchangeArray(sarr,_left,mid);
+            //接下来递归排序基准左右两边的数组
+            sort(sarr, left, _left-1);
+            sort(sarr, _left+1, right);
         }
-    });
-    return quickSort(left).concat(datum,quickSort(right));
+    }
+    //复制一个新的数组，后面返回排序过的新数组
+    let _arr=arr.concat();
+    sort(_arr);
+    return _arr;
 }
+
+/**
+ * @description: 快速排序-降序,快速排序算法是不稳定的算法
+ * @param {Array} 
+ * @return: Array
+ */
+function quickSortDesc(arr) {
+    let sort=function(sarr, left=0, right=arr.length-1){
+        if(left<right && sarr.length>1){
+            let _left=left,
+                _right=right+1,
+                mid=Math.floor((left+right)/2),
+                pivot = sarr[mid];
+            for(let i=left;i<_right;i++){
+                if(sarr[i]>pivot){
+                    _left!=i && exchangeArray(sarr,_left,i);
+                    if(_left==mid){
+                        mid=i;
+                    }
+                    ++_left;
+                }
+            }
+            exchangeArray(sarr,_left,mid);
+            sort(sarr, left, _left-1);
+            sort(sarr, _left+1, right);
+        }
+    }
+    let _arr=arr.concat();
+    sort(_arr);
+    return _arr;
+}
+
 export {
-    quickSort
+    quickSortAsc,
+    quickSortDesc,
+    verifyArrayOrderlyAsc,
+    verifyArrayOrderlyDesc
 };
--- vuees/src/views/quickSort.vue
+++ vuees/src/views/quickSort.vue
@@ -4,15 +4,36 @@
 </template>
 
 <script>
-import {quickSort} from '../assets/js/sort.js'
+import {quickSortAsc,quickSortDesc,verifyArrayOrderlyAsc,verifyArrayOrderlyDesc} from '../assets/js/sort.js'
 export default {
   name : "quickSort",
   methods : {
   },
   mounted(){
-    let arr = [1,4,456,2,2134,5,56,67,435,23,3,3,33,34,42,32432,23,2];
-    let sortArr = quickSort(arr);
-    console.log(sortArr)
+    let arr = [];
+    for(let i=0;i<1000000;i++){
+      arr.push(Math.floor(Math.random()*1000000));
+    }
+    let arr1=arr.concat(),
+      arr2=arr.concat(),
+      arr3=arr.concat();
+    console.time('quickSortAsc');
+    arr1=quickSortAsc(arr1);
+    console.timeEnd('quickSortAsc');
+    console.time('quickSortDesc');
+    arr2=quickSortDesc(arr2);
+    console.timeEnd('quickSortDesc');
+    console.time('quickSort3');
+    arr3.sort(function(pre,next){
+      return pre-next;
+    })
+    console.timeEnd('quickSort3');
+    console.log(verifyArrayOrderlyAsc(arr1));
+    console.log(verifyArrayOrderlyDesc(arr2));
+    console.log(verifyArrayOrderlyAsc(arr3));
+    let arr4=[1,78,32,1,9,34,23,65,3423,4353,12,67,12,5];
+    console.log(quickSortAsc(arr4));
+    console.log(quickSortDesc(arr4));
   }
 };
 </script>
