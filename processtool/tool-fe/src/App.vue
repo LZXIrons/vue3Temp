2eb867734302c101fc930353f89effbd78878878
liu_yong <liuyongfov@163.com>
2021-01-06 22:48:34 +0800

    feat: 系统工具项目-前端项目初步修改

--- processtool/tool-fe/src/App.vue
+++ processtool/tool-fe/src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="pageContainer">
+    <left-menu />
+    <keep-alive>
+      <router-view />
+    </keep-alive>
   </div>
-  <router-view />
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import LeftMenu from "@/components/LeftMenu.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "App",
+  components: {
+    "left-menu": LeftMenu
+  }
+});
+</script>
+
 <style lang="scss">
+@import "@/assets/style/_reset.scss";
+html,
+body,
 #app {
+  width: 100%;
+  height: 100%;
+}
+</style>
+<style scoped lang="scss">
+.pageContainer {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  display: flex;
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ processtool/tool-fe/src/components/LeftMenu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="leftMenu">
+    <router-link class="leftMenuLink" to="/">首页</router-link>
+    <router-link class="leftMenuLink" to="/about">说明</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LeftMenu"
+});
+</script>
+
+<style scoped lang="scss">
+.leftMenu {
+  padding: 30px 10px;
+  flex: 0 0 150px;
+  display: flex;
+  flex-direction: column;
+  .leftMenuLink {
+    font-weight: bold;
+    color: #2c3e50;
+    line-height: 42px;
+    text-decoration: none;
+    &:hover {
+      color: #000913;
+      text-decoration: underline;
+    }
+    &.router-link-exact-active {
+      color: #42b983;
+      cursor: default;
+      &:hover {
+        color: #42b983;
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
