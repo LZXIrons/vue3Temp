3e02808f4f51915c1c15d1a60a4c458972a242a0
liuyong <liuyongfov@163.com>
2021-09-15 10:32:48 +0800

    build:vite alias 配置、测试

--- vite2vue3study/src/views/About.vue
+++ vite2vue3study/src/views/About.vue
@@ -2,17 +2,20 @@
   <div class="about">
     <h1>about page</h1>
     {{ getTest }}
+    <HelloWorld :msg="'msg:' + Math.random()"></HelloWorld>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { mapGetters } from 'vuex';
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'About',
   computed: {
-    ...mapGetters(['getTest']),
+    ...mapGetters(['getTest'])
   },
-  methods: {},
+  components: { HelloWorld },
+  methods: {}
 });
 </script>
