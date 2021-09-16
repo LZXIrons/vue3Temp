0b63718b50ae418368dee54bbf0487ea77b5a6db
liuyong <liuyongfov@163.com>
2021-09-16 14:38:51 +0800

    build:配置scss并添加scss使用示例

--- vite2vue3study/src/components/HelloWorld.vue
+++ vite2vue3study/src/components/HelloWorld.vue
@@ -7,24 +7,31 @@ const count = ref(0);
 </script>
 
 <template>
+	<p class="title">title title</p>
+	<p class="bigtxt">big txt</p>
 	<p>{{ msg }}</p>
 	<button type="button" @click="count++">count is: {{ count }}</button>
 </template>
 
-<style scoped>
-a {
-	color: #42b983;
-}
+<style scoped lang="scss">
+@import '@/assets/scss/variable.scss';
+@import '@/assets/scss/mixin.scss';
 
-label {
-	margin: 0 0.5em;
-	font-weight: bold;
+p,
+button {
+	font-size: $font-size-txtm;
 }
-
-code {
-	background-color: #eee;
-	padding: 2px 4px;
-	border-radius: 4px;
-	color: #304455;
+p {
+	@include text-overflow(3);
+	&.bigtxt {
+		font-size: $font-size-btxtl;
+	}
+	&.title {
+		font-size: $font-size-titles;
+		@include userselect($page-user-select);
+	}
+}
+button {
+	@include box-shadow;
 }
 </style>
--- vite2vue3study/vite.config.ts
+++ vite2vue3study/vite.config.ts
@@ -8,5 +8,20 @@ export default defineConfig({
 		alias: {
 			'@': '/src'
 		}
+	},
+	css: {
+		//css预处理
+		preprocessorOptions: {
+			scss: {
+				/*
+				引入var.scss全局预定义变量，
+				如果引入多个文件，
+				可以使用
+				'@import "@/assets/scss/globalVariable1.scss";@import "@/assets/scss/globalVariable2.scss";'
+				这种格式
+				 */
+				additionalData: '@import "@/assets/scss/globalVariable.scss";'
+			}
+		}
 	}
 });
