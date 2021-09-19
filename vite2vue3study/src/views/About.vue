0b760239af35d206ca84cf2446d1eb39661286c9
liuyong <liuyongfov@163.com>
2021-09-19 13:17:55 +0800

    build:添加环境变量

--- vite2vue3study/src/views/About.vue
+++ vite2vue3study/src/views/About.vue
@@ -16,6 +16,13 @@ export default defineComponent({
 		...mapGetters(['getTest'])
 	},
 	components: { HelloWorld },
-	methods: {}
+	methods: {},
+	created() {
+		console.log('MODE:' + import.meta.env.MODE);
+		console.log('BASE_URL:' + import.meta.env.BASE_URL);
+		console.log('PROD:' + import.meta.env.PROD);
+		console.log('DEV:' + import.meta.env.DEV);
+		console.log('VITE_APP_TITLE:' + import.meta.env.VITE_APP_TITLE);
+	}
 });
 </script>
