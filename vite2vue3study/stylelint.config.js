e78a8b6a127892d6078ebc5ced680ea4e46601c1
liuyongfov@163.com <liuyongfov@163.com>
2021-09-24 15:54:07 +0800

    build:scss 使用mixin，stylelint报错问题

--- vite2vue3study/stylelint.config.js
+++ vite2vue3study/stylelint.config.js
@@ -8,7 +8,7 @@ module.exports = {
 		'at-rule-no-unknown': [
 			true,
 			{
-				ignoreAtRules: ['include']
+				ignoreAtRules: ['include', 'mixin']
 			}
 		],
 		'rule-empty-line-before': ['never']
