ed26fb4ea327c2882c5e6e44f48718a365f695cb
liuyong <liuyonfov@163.com>
2021-09-26 19:00:30 +0800

    build:stylelint order无效问题修复

--- vite2vue3study/stylelint.config.js
+++ vite2vue3study/stylelint.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	root: true,
-	plugins: ['stylelint-order', 'stylelint-scss'],
-	extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+	plugins: ['stylelint-scss'],
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-recess-order',
+		'stylelint-config-prettier'
+	],
 	rules: {
 		'declaration-colon-space-before': 'never',
 		'declaration-colon-space-after': 'always-single-line',
