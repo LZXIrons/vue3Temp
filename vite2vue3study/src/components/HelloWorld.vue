c0c22b1ef01b4bbbfc1caf21290bbead04416e38
liuyongfov@163.com <liuyongfov@163.com>
2021-10-08 09:37:49 +0800

    feat:增加axios功能

--- vite2vue3study/src/components/HelloWorld.vue
+++ vite2vue3study/src/components/HelloWorld.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
 defineProps({ msg: String });
-
+onMounted(() => {
+	axios.get(import.meta.env.VITE_INTERFACE_ORIGIN + 'osinfo/all').then(res => {
+		console.log(res.data);
+	});
+});
 const count = ref(0);
 </script>
 
--- vite2vue3study/vite.config.ts
+++ vite2vue3study/vite.config.ts
@@ -3,6 +3,17 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
+		proxy: {
+			'^/interface/': {
+				target: 'http://localhost:8000/',
+				changeOrigin: true,
+				rewrite: path => path.replace(/^\/interface/, '')
+			}
+		}
+	},
 	plugins: [vue()],
 	resolve: {
 		alias: {
