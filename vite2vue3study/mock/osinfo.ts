f051c88669388d231e05ebb22ca3bfa3f0891c64
liuyong <liuyonfov@163.com>
2021-10-10 17:54:04 +0800

    feat:增加mock功能

---
+++ vite2vue3study/mock/osinfo.ts
@@ -0,0 +1,19 @@
+// 仅做示例: 通过GET请求返回一个名字数组
+// https://www.npmjs.com/package/vite-plugin-mock
+export default [
+	{
+		url: '/osinfo/all',
+		method: 'get',
+		response: (): {
+			code: number;
+			message: string;
+			data: string[];
+		} => {
+			return {
+				code: 0,
+				message: 'ok',
+				data: ['tom', 'jerry']
+			};
+		}
+	}
+];
--- vite2vue3study/vite.config.ts
+++ vite2vue3study/vite.config.ts
@@ -1,38 +1,49 @@
-import { defineConfig } from 'vite';
+import { defineConfig, ConfigEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { viteMockServe } from 'vite-plugin-mock';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	server: {
-		host: '0.0.0.0',
-		port: 3000,
-		proxy: {
-			'^/interface/': {
-				target: 'http://localhost:8000/',
-				changeOrigin: true,
-				rewrite: path => path.replace(/^\/interface/, '')
+export default defineConfig(({ command }: ConfigEnv) => {
+	return {
+		server: {
+			host: '0.0.0.0',
+			port: 3000,
+			proxy: {
+				'^/interface/': {
+					target: 'http://localhost:3000/',
+					changeOrigin: true,
+					rewrite: path => path.replace(/^\/interface/, '')
+				}
 			}
-		}
-	},
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			'@': '/src'
-		}
-	},
-	css: {
-		//css预处理
-		preprocessorOptions: {
-			scss: {
-				/*
+		},
+		plugins: [
+			vue(),
+			viteMockServe({
+				supportTs: true,
+				mockPath: 'mock',
+				localEnabled: command === 'serve',
+				watchFiles: true
+			})
+		],
+		resolve: {
+			alias: {
+				'@': '/src'
+			}
+		},
+		css: {
+			//css预处理
+			preprocessorOptions: {
+				scss: {
+					/*
 				引入var.scss全局预定义变量，
 				如果引入多个文件，
 				可以使用
 				'@import "@/assets/scss/globalVariable1.scss";@import "@/assets/scss/globalVariable2.scss";'
 				这种格式
 				 */
-				additionalData: '@import "@/assets/scss/globalVariable.scss";'
+					additionalData: '@import "@/assets/scss/globalVariable.scss";'
+				}
 			}
 		}
-	}
+	};
 });
