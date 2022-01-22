cf2264aeb7b4787e38beb5c9fd0d727cd2d8c0cc
lzx <lzx>
2022-01-22 22:06:48 +0800

    签约

--- vue3Init/src/App.vue
+++ vue3Init/src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <router-view />
+	<router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {}
+	name: 'App',
+	components: {}
 })
 </script>
 
 <style>
 #app {
-  margin-top: 60px;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  color: #2c3e50;
-  text-align: center;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+	margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	color: #2c3e50;
+	text-align: center;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
 }
 </style>
